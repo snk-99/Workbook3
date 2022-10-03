714e3f41d008b9ace2262e4a1d7358e90b3a78da
Samuel Kahura <kahurasamuel@gmail.com>
2022-10-03 11:12:38 -0400

    favorite things function

---
+++ Functions/simple_functions.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function favoriteThings() {
+    const fullName = "Samuel Kahura";
+    const favoriteMovie = "The Gentelmen";
+    const favoriteBand = "Sauti Sol";
+
+    console.log(
+        `My name is ${fullName}. My favorite movie is ${favoriteMovie}, and my favorite band is ${favoriteBand}`
+    );
+}
+
+favoriteThings();
